a8cf34d30316ae98ab4ef0f81c5433ce09ebd02d
Charles Machalow <csm10495@gmail.com>
2020-07-01 21:15:21 -0700

    allow pyinstaller --onefile to work (at least on win32)

--- go_timer.py
+++ go_timer.py
@@ -9,20 +9,23 @@ from listener.gamestate_listener import ListenerWrapper
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 CFG_FILE = os.path.join(THIS_DIR, 'listener', 'gamestate_integration_go_timer.cfg')
 
-def ensure_cfg_file_is_there():
-    # todo... if needed find out more csgo locations
-    possible_paths = ['~/.steam/steamapps/common/Counter-Strike Global Offensive/',
-                     r"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive"]
-    for p_str in possible_paths:
-        p = pathlib.Path(p_str).expanduser()
-        if p.is_dir():
-            copy_to_path = p / "csgo" / "cfg"
-            if copy_to_path.is_file():
-                copy_to_path.unlink()
+multiprocessing.freeze_support()
 
-            shutil.copy(CFG_FILE, copy_to_path)
-            print ("Copied %s -> %s" % (CFG_FILE, copy_to_path))
-            return True
+def ensure_cfg_file_is_there():
+    if os.path.isfile(CFG_FILE):
+        # todo... if needed find out more csgo locations
+        possible_paths = ['~/.steam/steamapps/common/Counter-Strike Global Offensive/',
+                            r"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive"]
+        for p_str in possible_paths:
+            p = pathlib.Path(p_str).expanduser()
+            if p.is_dir():
+                copy_to_path = p / "csgo" / "cfg"
+                if copy_to_path.is_file():
+                    copy_to_path.unlink()
+
+                shutil.copy(CFG_FILE, copy_to_path)
+                print ("Copied %s -> %s" % (CFG_FILE, copy_to_path))
+                return True
 
     return False
 
--- gui/bomb_timer_gui.py
+++ gui/bomb_timer_gui.py
@@ -100,7 +100,8 @@ class TimerGui(multiprocessing.Process):
         current_dir = os.path.dirname(os.path.realpath(__file__))
         parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
         if "nt" == os.name:
-            self.root.wm_iconbitmap(bitmap = os.path.join(parent_dir, "gt_icon.ico"))
+            if os.path.exists(os.path.join(parent_dir, "gt_icon.ico")):
+                self.root.wm_iconbitmap(bitmap = os.path.join(parent_dir, "gt_icon.ico"))
 
         self.gui = TimerCanvas(self.root, TIMER_WIDTH, TIMER_HEIGHT)
         self.gui.configure(background=BG_COLOUR)
